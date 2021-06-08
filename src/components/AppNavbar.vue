53e5f9853d92f696c520e17211720f78bb87a8cf
Dioxo1495 <joshhenriquez95@gmail.com>
2021-06-07 22:26:18 -0400

    Josh

---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ul class="nav justify-content-center">
+            <li class="nav-item">
+                <router-link to="/" class="nav-link active" aria-current="page" href="#">Active</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
