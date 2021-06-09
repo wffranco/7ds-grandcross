5ece23946cfd08f0884d55a94069fe601c4a2509
Willem Franco <willemfranco@gmail.com>
2021-06-09 15:11:54 -0500

    Agregando firebase

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app';
+// import 'firebase/analytics';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCFazW26175vIwGPIdYBgBdh5PQASz9mZE',
+  authDomain: 'grand-cross-7ds.firebaseapp.com',
+  projectId: 'grand-cross-7ds',
+  storageBucket: 'grand-cross-7ds.appspot.com',
+  messagingSenderId: '694417478012',
+  appId: '1:694417478012:web:b0b9ccd40e499bd00c54f9',
+  measurementId: 'G-5913LH1RW5',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore();
+export const auth = firebase.auth();
+export const storage = firebase.storage();
--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import './registerServiceWorker';
 import router from './router';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { db } from './firebase';
+
+console.log('DB:', db);
 
 createApp(App).use(router).mount('#app');
