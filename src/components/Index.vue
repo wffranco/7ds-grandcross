2f9bc8099cc752309d26f2687033dfc80e6b269b
Dioxo1495 <joshhenriquez95@gmail.com>
2021-06-12 16:52:46 -0400

    LogIn/LogOut Layout

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="position-absolute top-50 start-50 translate-middle">
-    <button v-if="!user" class="btn btn-dark text-white" @click="login">Iniciar Sesion con Google</button>
+    <!-- <button v-if="!user" class="btn btn-dark text-white" @click="login">Iniciar Sesion con Google</button>
     <div v-else>
       <h1>Bienvenido, {{ user.displayName }}</h1>
       <button class="btn btn-danger text-white mt-2 mx-auto" @click="logout">Cerrar Sesion</button>
-    </div>
+    </div> -->
   </div>
 </template>
 
