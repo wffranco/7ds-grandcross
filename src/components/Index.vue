72e02231cf22203fdbdb1bd5519a8ef7e8a87dd4
Dioxo1495 <joshhenriquez95@gmail.com>
2021-06-12 16:56:18 -0400

    Welcome Msg LogIn

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="position-absolute top-50 start-50 translate-middle">
-    <!-- <button v-if="!user" class="btn btn-dark text-white" @click="login">Iniciar Sesion con Google</button>
+    <div v-if="!user">
+      <h1>HOME</h1>
+    </div>
     <div v-else>
       <h1>Bienvenido, {{ user.displayName }}</h1>
-      <button class="btn btn-danger text-white mt-2 mx-auto" @click="logout">Cerrar Sesion</button>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script>
-import { login, logout, useUser } from '@/store/auth';
+import { useUser } from '@/store/auth';
 export default {
   setup () {
     const user = useUser();
 
-    return { login, logout, user };
+    return { user };
   },
 };
 </script>
